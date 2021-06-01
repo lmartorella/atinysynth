10a2a3178dc8c8eee42bb0a09306ccbbf7acd8af
Luciano Martorella <mad_lux_it@yahoo.it>
2021-06-01 22:09:24 +0200

    - Tetris

--- tune_gen.h
+++ tune_gen.h
@@ -1,12 +1,12 @@
 #include "sequencer.h"
 
-// Tune: resources/loreley.mml
+// Tune: resources/tetris.mml
 
 struct tune_frame_t {
 	uint8_t adsr_time_scale : 3;
 	uint8_t wf_period : 5;
 	uint8_t wf_amplitude : 1;
-	uint8_t adsr_release_start : 2;
+	uint8_t adsr_release_start : 1;
 };
 
 extern const uint16_t tune_adsr_time_scale_refs[];
